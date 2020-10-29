eee01e393e05cb3be6f13dd3d56376fa687279cb
Anh Trần <insac81196@gmail.com>
2020-10-30 02:22:18 +0700

    Heroku

--- config/app.config.js
+++ config/app.config.js
@@ -1,5 +1,6 @@
 export default {
-    database: 'mongodb://localhost:27017',
+    //database: 'mongodb://localhost:27017',
+    database: 'mongodb://admin:cf1996@ds363088.mlab.com:63088/thinkpro',
     jwt: '961108',
     imgur: 'Client-ID 1c3db41bb73b992'
 }
