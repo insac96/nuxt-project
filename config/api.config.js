c8c20abedab81096254772679b9a7b76af3dfd71
Anh Trần <insac81196@gmail.com>
2020-11-25 09:32:33 +0700

    update

--- config/api.config.js
+++ config/api.config.js
@@ -2,6 +2,6 @@ export default {
     //MongoDB Link Path
     database: 'mongodb://admin:cf1996@ds363088.mlab.com:63088/thinkpro',
 
-    //Imgur API ID
-    imgur: 'Client-ID 1c3db41bb73b992'
+    //JWT Secret
+    jwt: '1c3db41bb73b992'
 }
